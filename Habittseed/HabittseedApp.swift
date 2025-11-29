cdf824e707c73ec6933defb97f83812a77bc95a5
sumi <haruto@sumi.tv>
2025-11-29 17:38:30 +0900

    Initial Commit

---
+++ Habittseed/HabittseedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabittseedApp.swift
+//  Habittseed
+//
+//  Created by はると on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct HabittseedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
